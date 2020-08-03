20685903c8e272909980f5fb4b6414ba7150698d
hshine1226 <hshine1226@gmail.com>
2020-08-03 23:20:14 +0900

    Add material-ui theme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
 import GlobalStyles from "Components/GlobalStyles";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Noto Sans KR",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <>
         <GlobalStyles />
-        <Router />
+        <ThemeProvider theme={theme}>
+          <Router />
+        </ThemeProvider>
       </>
     );
   }
