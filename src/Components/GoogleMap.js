29404234299a984ed049f7fa8696c990fc8adc23
hshine1226 <hshine1226@gmail.com>
2020-08-03 23:21:08 +0900

    Add getting current location logic

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -4,6 +4,9 @@ import Loader from "./Loader";
 import styled from "styled-components";
 import Fab from "@material-ui/core/Fab";
 import NavigationIcon from "@material-ui/icons/Navigation";
+import AddIcon from "@material-ui/icons/Add";
+import Tooltip from "@material-ui/core/Tooltip";
+import TextField from "@material-ui/core/TextField";
 
 const Container = styled.div`
   width: 100%;
@@ -19,6 +22,9 @@ export class GoogleMap extends Component {
       loading: false,
       stores: [],
       center: { lat: 36.3142511, lng: 127.45745780000001 },
+      activeMarker: {},
+      showingInfoWindow: false,
+      markerCenter: null,
     };
   }
 
@@ -47,18 +53,55 @@ export class GoogleMap extends Component {
   }
 
   handleCurrentPosition = async () => {
+    if ("geolocation" in navigator) {
+      const success = async (pos) => {
+        const {
+          coords: { latitude: lat, longitude: lng },
+        } = pos;
+
+        await this.setState({
+          center: { lat, lng },
+          markerCenter: { lat, lng },
+        });
+      };
+
+      const error = (err) => {
+        this.setState({
+          center: { lat: 36.3142511, lng: 127.45745780000001 },
+          markerCenter: { lat: 36.3142511, lng: 127.45745780000001 },
+        });
+      };
+
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log("Not Available");
+    }
+  };
+
+  onMarkerClick = (props, marker, e) => {
+    console.log(marker);
+
     this.setState({
-      center: { lat: 36.3142511, lng: 127.45745780000001 },
+      activeMarker: marker,
+      showingInfoWindow: true,
+      markerCenter: marker.position,
     });
   };
 
   render() {
-    const { loading, center } = this.state;
+    const { loading, center, markerCenter } = this.state;
 
     const iconStyle = {
       zIndex: 3,
       position: "absolute",
       bottom: "200px",
+      right: "4px",
+    };
+
+    const addStyle = {
+      zIndex: 3,
+      position: "absolute",
+      bottom: "270px",
       right: "4px",
     };
 
@@ -74,7 +117,24 @@ export class GoogleMap extends Component {
           center={center}
           zoom={14}
           onClick={this.addMarkers}
-        ></Map>
+        >
+          <Marker
+            name={"내위치"}
+            position={markerCenter}
+            icon={{
+              url: "https://image.flaticon.com/icons/png/512/66/66884.png",
+              anchor: new this.props.google.maps.Point(40, 40),
+              scaledSize: new this.props.google.maps.Size(40, 40),
+            }}
+            draggable={true}
+            onClick={this.onMarkerClick}
+          />
+          <InfoWindow marker={this.state.activeMarker} visible={true}>
+            <div>
+              <p>{this.state.activeMarker.name}</p>
+            </div>
+          </InfoWindow>
+        </Map>
         <Fab
           color="primary"
           aria-label="add"
@@ -83,6 +143,11 @@ export class GoogleMap extends Component {
         >
           <NavigationIcon />
         </Fab>
+        <Tooltip title="화장실 추가" aria-label="add" style={addStyle}>
+          <Fab color="primary">
+            <AddIcon />
+          </Fab>
+        </Tooltip>
       </Container>
     );
   }
