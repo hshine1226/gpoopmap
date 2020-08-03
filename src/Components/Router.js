125280f9badecb761cd5d3b5d6f0090b72b7358f
hshine1226 <hshine1226@gmail.com>
2020-08-03 23:22:00 +0900

    Add input test component

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -10,6 +10,7 @@ import Home from "Routes/Home";
 import Login from "./Login";
 import Join from "./Join";
 import Header from "./Header";
+import Input from "./Input";
 
 export default () => (
   <Router>
@@ -19,6 +20,7 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} />
         <Route path="/join" exact component={Join} />
+        <Route path="/input" exact component={Input} />
 
         {/* 일치하는 route가 없다면  Home으로 Redirect 시킨다.*/}
         <Redirect from="*" to="/" />
