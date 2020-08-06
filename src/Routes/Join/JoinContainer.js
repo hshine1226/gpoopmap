dc69c358bf3c07b4b90d1bf322a3b2320392a65a
hshine1226 <hshine1226@gmail.com>
2020-08-06 20:53:03 +0900

    Add join container & presenter

--- src/Routes/Join/JoinContainer.js
+++ src/Routes/Join/JoinContainer.js
@@ -2,11 +2,15 @@ import React from "react";
 import JoinPresenter from "./JoinPresenter";
 
 export default class extends React.Component {
-  state = {
-    loading: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+
   render() {
     const { loading } = this.state;
-    return <JoinPresenter loading={loading} />;
+    return <JoinPresenter loading={loading} history={this.props.history} />;
   }
 }
--- src/Routes/Join/JoinPresenter.js
+++ src/Routes/Join/JoinPresenter.js
@@ -2,16 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Loader from "../../Components/Loader";
-import Join from "../../Components/Join.js";
+import Join from "../../Components/Join";
 
 const Container = styled.div``;
 
-const JoinPresenter = ({ loading }) =>
+const JoinPresenter = ({ loading, history }) =>
   loading ? (
     <Loader />
   ) : (
     <Container>
-      <Join />
+      <Join history={history} />
     </Container>
   );
 
