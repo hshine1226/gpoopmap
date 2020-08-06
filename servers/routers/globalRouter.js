d44af71dcb71b39ef397a2df2661822f9d95a642
hshine1226 <hshine1226@gmail.com>
2020-08-06 20:52:21 +0900

    Add login router

--- servers/routers/globalRouter.js
+++ servers/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import routes from "../routes";
-
-import { postJoin } from "../controllers/userController";
+import { postJoin, postLogin } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.post(routes.join, postJoin);
+globalRouter.post(routes.login, postLogin);
 
 export default globalRouter;
