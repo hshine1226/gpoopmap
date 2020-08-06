071196e51758cfda26179e678d1d331e4ada1b7d
hshine1226 <hshine1226@gmail.com>
2020-08-07 03:10:42 +0900

    Add postToilet logic
    to save new toilet

--- servers/controllers/apiController.js
+++ servers/controllers/apiController.js
@@ -1,3 +1,27 @@
+import Toilet from "../models/Toilet";
+
 export const getMe = async (req, res) => {
   res.send({ user: req.user });
+};
+
+export const postToilet = async (req, res) => {
+  const {
+    body: { lat, lng, name, type },
+  } = req;
+
+  try {
+    const toilet = await Toilet({
+      type,
+      name,
+      location: {
+        coordinates: [lat, lng],
+      },
+    });
+    toilet.save();
+  } catch (error) {
+    res.status(400);
+    console.log(error);
+  } finally {
+    res.end();
+  }
 };
