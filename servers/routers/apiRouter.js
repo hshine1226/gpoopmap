fb2c6b58cdc0481e7057a93bcc9c1b8fc948675c
hshine1226 <hshine1226@gmail.com>
2020-08-07 12:54:59 +0900

    Add routers

--- servers/routers/apiRouter.js
+++ servers/routers/apiRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
 import routes from "../routes";
-import { getMe } from "../controllers/apiController";
+import {
+  getMe,
+  postToilet,
+  postNearToilets,
+} from "../controllers/apiController";
 
 const apiRouter = express.Router();
 
 apiRouter.get(routes.me, getMe);
+apiRouter.post(routes.toilet, postToilet);
+apiRouter.post(routes.near, postNearToilets);
 
 export default apiRouter;
