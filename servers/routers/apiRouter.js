19833863773a3804e48088154bee8298c6fc0d36
hshine1226 <hshine1226@gmail.com>
2020-08-08 14:05:51 +0900

    Move global router to api router

--- servers/routers/apiRouter.js
+++ servers/routers/apiRouter.js
@@ -3,13 +3,19 @@ import routes from "../routes";
 import {
   getMe,
   postToilet,
-  postNearToilets,
+  postJoin,
+  postLogin,
+  logout,
+  getNearToilets,
 } from "../controllers/apiController";
 
 const apiRouter = express.Router();
 
+apiRouter.post(routes.join, postJoin);
+apiRouter.post(routes.login, postLogin);
+apiRouter.get(routes.logout, logout);
 apiRouter.get(routes.me, getMe);
-apiRouter.post(routes.toilet, postToilet);
-apiRouter.post(routes.near, postNearToilets);
+apiRouter.post(routes.postToilet, postToilet);
+apiRouter.get(routes.nearToilets, getNearToilets);
 
 export default apiRouter;
--- servers/routers/globalRouter.js
+++ servers/routers/globalRouter.js
@@ -1,11 +1,5 @@
 import express from "express";
-import routes from "../routes";
-import { postJoin, postLogin, logout } from "../controllers/userController";
 
 const globalRouter = express.Router();
-
-globalRouter.post(routes.join, postJoin);
-globalRouter.post(routes.login, postLogin);
-globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
