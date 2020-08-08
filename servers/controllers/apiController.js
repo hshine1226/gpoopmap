656452bb276e054709955233ec978ce70635c3ae
hshine1226 <hshine1226@gmail.com>
2020-08-08 14:06:13 +0900

    Move user controller to api controller

--- servers/controllers/apiController.js
+++ servers/controllers/apiController.js
@@ -1,4 +1,62 @@
+import passport from "passport";
 import Toilet from "../models/Toilet";
+import User from "../models/User";
+
+export const postJoin = async (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+
+  console.log(req.body);
+
+  const user = await User.findOne({ email });
+
+  if (password !== password2) {
+    // 패스워드 불일치
+    res.send({ success: false, error: "Invalidpassword" });
+  } else if (user) {
+    res.send({ success: false, error: "userExist" });
+  } else {
+    try {
+      const user = await User({ name, email });
+      await User.register(user, password);
+      res.send({ success: true });
+    } catch (err) {
+      res.status(400);
+    }
+  }
+};
+
+export const postLogin = (req, res) => {
+  passport.authenticate("local", (error, user, info) => {
+    if (error) {
+      console.log(error);
+    }
+    if (user) {
+      console.log(user);
+      req.login(user, (error) => {
+        if (error) {
+          console.log(error);
+        }
+        res.send({ success: true });
+      });
+    } else {
+      res.send({ success: false });
+    }
+  })(req, res);
+};
+
+// export const postLogin = passport.authenticate("local");
+
+export const logout = (req, res) => {
+  try {
+    req.logout();
+    console.log("로그아웃...");
+    res.send({ success: true });
+  } catch {
+    res.send({ success: false });
+  }
+};
 
 export const getMe = async (req, res) => {
   res.send({ user: req.user });
@@ -10,6 +68,7 @@ export const postToilet = async (req, res) => {
   } = req;
 
   try {
+    // 로그인한 유저만 화장실 등록 가능
     if (req.user) {
       const toilet = await Toilet({
         type,
@@ -36,25 +95,27 @@ export const postToilet = async (req, res) => {
   }
 };
 
-export const postNearToilets = async (req, res) => {
+export const getNearToilets = async (req, res) => {
   const {
-    body: { lng, lat },
+    query: { lat, lng },
   } = req;
 
-  try {
-    const toilet = await Toilet.find({
-      location: {
-        $near: {
-          $maxDistance: 1000,
-          $geometry: {
-            type: "Point",
-            coordinates: [lng, lat],
-          },
-        },
-      },
-    });
-    res.send(toilet);
-  } catch (error) {
-    console.log(error);
-  }
+  console.log(lat, lng);
+  // try {
+  //   const toilet = await Toilet.find({
+  //     location: {
+  //       $near: {
+  //         $maxDistance: 1000,
+  //         $geometry: {
+  //           type: "Point",
+  //           coordinates: [lng, lat],
+  //         },
+  //       },
+  //     },
+  //   }).populate("creator");
+  //   console.log(toilet);
+  //   res.send(toilet);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 };
