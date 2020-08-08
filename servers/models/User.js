c365c11f3776c21b1a538a2d556d7b90cf4e1b26
hshine1226 <hshine1226@gmail.com>
2020-08-08 14:08:08 +0900

    Add avatarUrl

--- servers/models/User.js
+++ servers/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   toilets: [{ type: mongoose.Schema.Types.ObjectId, ref: "Toilet" }],
+  avatarUrl: String,
 });
 
 userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
