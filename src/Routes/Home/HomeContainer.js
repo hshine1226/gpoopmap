ab1e5b02c5c9a2d25df77be2c2d3564c9ed7e4a6
hshine1226 <hshine1226@gmail.com>
2020-08-08 14:08:57 +0900

    Add props.history to the HomePresenter

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -2,13 +2,17 @@ import React from "react";
 import HomePresenter from "./HomePresenter";
 
 export default class extends React.Component {
-  state = {
-    loading: false,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+    };
+  }
 
   render() {
     const { loading } = this.state;
 
-    return <HomePresenter loading={loading} />;
+    return <HomePresenter loading={loading} history={this.props.history} />;
   }
 }
