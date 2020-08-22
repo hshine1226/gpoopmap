273fcaac55bab2a459cb2ba797b7b891958e2fb2
hshine1226 <hshine1226@gmail.com>
2020-08-22 22:13:10 +0900

    Set store path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "Components/App";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./store/";
+import rootReducer from "./store";
 
 const store = configureStore({ reducer: rootReducer });
 
