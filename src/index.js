b2a81b5d04a999883c489810c7546c754f89d242
hshine1226 <hshine1226@gmail.com>
2020-08-23 18:22:20 +0900

    Update App root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "Components/App";
+import App from "./Components/App";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./store";
